df592ef88a69fed297e47659f5b2c03190920148
Olena Penn <olenapenn@gmail.com>
2018-03-21 11:45:36 -1000

    basic server and file structure with dependencies

---
+++ app/server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const session = require('express-session');
+const methodOverride = require('method-override');
+
+app.use(methodOverride('_method'));
+app.use(session({
+    secret:'feedmeseymour',
+    resave:false,
+    saveUninitialized: false
+}))
+app.use(express.urlencoded({extended:false}));
+
+app.get("/", (req, res)  =>  {
+  res.send("hi");
+})
+
+
+
+
+mongoose.connect('mongodb://localhost:27017/treats');
+
+mongoose.connection.once('open', ()=>{
+    console.log('connected to mongo');
+})
+
+app.listen(3000, ()=>{
+    console.log('listening');
+});
